31845d60216f3a1ff112d9ec6ffdc39c9c606551
curly08 <mattynoakes08@gmail.com>
2021-04-27 11:43:48 -0400

    Rework old code, add event listeners to buttons, add play again button that refreshes page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
-let userScore = 0;
-let computerScore = 0;
+const container = document.querySelector('#container');
+
+//create div containing round decision
+const roundDecision = document.createElement('div');
+roundDecision.setAttribute('id', 'roundDecision');
+container.appendChild(roundDecision);
+
+//create div containing the score
+const score = document.createElement('div');
+score.setAttribute('id', 'score');
+container.appendChild(score);
+
+//create div containing end game result and reset button?
+const endGame = document.createElement('div');
+endGame.setAttribute('id', 'endGame');
+container.appendChild(endGame);
+
+//declare buttons div
+const buttonsDiv = document.querySelector('#buttons');
 
 //computer selection
 function random() {
@@ -18,83 +35,78 @@ function random() {
     }
 }
 
-//player selection
-const userSelection = document.querySelectorAll('.button');
-userSelection.forEach((button) => {
-    button.addEventListener('click', playRound);
+//add event listener (click) to buttons that calls playRound()
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, random());
+    });
 });
 
-//check to see if someone gets 5 points and resets the game
-function checkForWinner() {
-    if (userScore == 5) {
-        endGame.textContent = 'You were the first to get 5 points! Congratulations!';
-        userScore = 0;
-        computerScore = 0;
-    } else if (computerScore == 5) {
-        endGame.textContent = 'The computer got 5 points first. :(';
-        userScore = 0;
-        computerScore = 0;
-    }
-}
+//declare userScore and compScore variables
+let userScore = 0;
+let compScore = 0;
 
 //play a round function
-function playRound() {
-    const compSelection = random();
+function playRound(userSelection, compSelection) {
     endGame.textContent = '';
 
-    if (this.id == 'rock' && compSelection == 'rock') {
+    if (userSelection == 'rock' && compSelection == 'rock') {
         roundDecision.textContent = 'You tied. You both chose rock.';
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'rock' && compSelection == 'paper') {
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'rock' && compSelection == 'paper') {
         roundDecision.textContent = 'You lost. Paper covers rock.';
-        computerScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'rock' && compSelection == 'scissors') {
+        compScore += 1;
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'rock' && compSelection == 'scissors') {
         roundDecision.textContent = 'You won. Rock crushes scissors.';
         userScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'paper' && compSelection == 'rock') {
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'paper' && compSelection == 'rock') {
         roundDecision.textContent = 'You won. Paper covers rock.';
         userScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'paper' && compSelection == 'paper') {
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'paper' && compSelection == 'paper') {
         roundDecision.textContent = 'You tied. You both chose paper.';
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'paper' && compSelection == 'scissors') {
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'paper' && compSelection == 'scissors') {
         roundDecision.textContent = 'You lost. Scissors cut up paper.';
-        computerScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'scissors' && compSelection == 'rock') {
+        compScore += 1;
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'scissors' && compSelection == 'rock') {
         roundDecision.textContent = 'You lost. Rock crushes scissors.';
-        computerScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'scissors' && compSelection == 'paper') {
+        compScore += 1;
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'scissors' && compSelection == 'paper') {
         roundDecision.textContent = 'You won. Scissors cut up paper.';
         userScore += 1;
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-    } else if (this.id == 'scissors' && compSelection == 'scissors') {
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
+    } else if (userSelection == 'scissors' && compSelection == 'scissors') {
         roundDecision.textContent = 'You tied. You both chose scissors.';
-        score.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
+        score.textContent = `Your score: ${userScore} Computer score: ${compScore}`;
     } else {
         return 'Round dismissed.';
     }
 
-    checkForWinner();
+    checkForWinner(userScore, compScore);
 }
 
-const container = document.querySelector('#container');
+//check to see if someone gets 5 points and resets the game
+function checkForWinner(userScore, compScore) {
+    if (userScore == 5) {
+        endGame.textContent = 'You were the first to get 5 points! Congratulations!';
+        container.removeChild(buttonsDiv);
+        container.appendChild(playAgainButton);
+    } else if (compScore == 5) {
+        endGame.textContent = 'The computer got 5 points first. :(';
+        container.removeChild(buttonsDiv);
+        container.appendChild(playAgainButton);
+    }
+}
 
-//create div containing round decision
-const roundDecision = document.createElement('div');
-roundDecision.setAttribute('id', 'roundDecision');
-container.appendChild(roundDecision);
-
-//create div containing the score
-const score = document.createElement('div');
-score.setAttribute('id', 'score');
-container.appendChild(score);
-
-//create div containing end game result and reset button?
-const endGame = document.createElement('div');
-endGame.setAttribute('id', 'endGame');
-container.appendChild(endGame);
+//play again button
+const playAgainButton = document.createElement('button');
+playAgainButton.textContent = 'Play Again';
+playAgainButton.addEventListener('click', () => {
+    location.reload();
+});
